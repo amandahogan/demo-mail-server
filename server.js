a922bfe2f062608be0da7c937d5882620c3d5314
amandahogan <hogan.ans@gmail.com>
2017-04-06 13:34:56 +1200

    console.log userAgent

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 module.exports = app
 
 app.get('/response', (req, res) => {
+  console.log(req.headers)
 
   if (req.query.key !== process.env.KEY) {
     res.status(403).send('Access denied.')
@@ -13,28 +14,28 @@ app.get('/response', (req, res) => {
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-          user: process.env.USERNAME,
-          pass: process.env.PASSWORD
-      }
+    service: 'gmail',
+    auth: {
+      user: process.env.USERNAME,
+      pass: process.env.PASSWORD
+    }
   })
 
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-      to: process.env.NOTIFIED_EMAIL, // list of receivers
-      subject: req.query.userId, // Subject line
-      text: req.query.proId, // plain text body
-      html: `<p>${JSON.stringify(req.query)}</p>` // html body
+    from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
+    to: process.env.NOTIFIED_EMAIL, // list of receivers
+    subject: req.query.userId, // Subject line
+    text: req.query.proId, // plain text body
+    html: `<p>${JSON.stringify(req.query)}</p>` // html body
   }
 
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error)
-      }
-      console.log('Message %s sent: %s', info.messageId, info.response)
-    })
-    res.redirect(process.env.REDIRECT)
+    if (error) {
+      return console.log(error)
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response)
+  })
+  res.redirect(process.env.REDIRECT)
 })
